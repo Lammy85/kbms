ec87e9a08ee327c83b4a553424f39967aa464a2d
Lammy85 <jammer_lammy@web.de>
2024-10-23 09:19:16 +0200

    GUI-Klasse bearbeitet: Kunde hinzufügen und showAlert methode

--- src/view/kbmsGUI.java
+++ src/view/kbmsGUI.java
@@ -1,20 +1,117 @@
 package view;
+
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.application.Application;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import model.Kunde;
+import services.BestellungService;
+import services.KundenService;
+
+import java.lang.classfile.Label;
 
 public class kbmsGUI extends Application {
-    Stage buehne;
+
+    private BestellungService bestellungService;
+    private KundenService kundenService;
+    private Stage buehne;
+
     @Override
-    public void start(Stage stage) throws Exception {
-        this.buehne = stage;
+    public void start(Stage primaryStage) throws Exception {
+        this.buehne = primaryStage;
+        bestellungService = new BestellungService();
+        kundenService = new KundenService();
+
+        showHauptmenue();
+    }
+
+    private void showHauptmenue() {
+
+        //Layout
+
         VBox kiste = new VBox(20);
         kiste.setPadding(new Insets(20));
-        Scene screen = new Scene(kiste,400,500);
-        buehne.setScene(screen);
+
+        Label titleLabel = new Label("KBMS");
+
+        Button addKunde = new Button("Kunde hinzufügen");
+        addKunde.setOnAction(e -> showAddKunde());
+
+        Button addBestellung = new Button("Bestellung hinzufügen");
+        addBestellung.setOnAction(e -> showAddBestellung());
+
+        kiste.getChildren().addAll(titleLabel, addKunde, addBestellung);
+
+        Scene scene = new Scene(kiste, 300, 200);
+        buehne.setTitle("KBMS - Hauptmenü");
+        buehne.setScene(scene);
         buehne.show();
+    }
+
+    private void showAddKunde() {
+
+        //Layout
+
+        VBox kiste = new VBox(20);
+        kiste.setPadding(new Insets(20));
+
+        //Inputs
+
+        TextField vornameFeld = new TextField();
+        vornameFeld.setPromptText("Vorname des Kunden eingeben");
+
+        TextField nachnameFeld = new TextField();
+        nachnameFeld.setPromptText("Nachname des Kunden eingeben");
+
+        TextField knrFeld = new TextField();
+        knrFeld.setPromptText("Kundennummer eingeben");
+
+        TextField strasseFeld = new TextField();
+        strasseFeld.setPromptText("Strasse des Kunden eingeben");
+
+        TextField hausnummerFeld = new TextField();
+        hausnummerFeld.setPromptText("Hausnummer des Kunden eingeben");
+
+        TextField plzFeld = new TextField();
+        plzFeld.setPromptText("Postleitzahl des Kunden eingeben");
+
+        TextField ortFeld = new TextField();
+        ortFeld.setPromptText("Wohnort des Kunden eingeben");
+
+        //Kunde hinzufügen button
+
+        Button addButton = new Button("Kunde hinzufügen");
+        addButton.setOnAction(e -> {
+            String vorname = vornameFeld.getText();
+            String nachname = nachnameFeld.getText();
+            int kundennummer = Integer.parseInt(knrFeld.getText());
+            String strasse = strasseFeld.getText();
+            int hausnummer = Integer.parseInt(hausnummerFeld.getText());
+            int plz = Integer.parseInt(plzFeld.getText());
+            String wohnort = ortFeld.getText();
+
+            if (kundennummer.isEmpty()){
+            showAlert("Fehler","Bitte Kundennummer vergeben");
+            }
+
+            Kunde kunde = new Kunde(vorname, nachname, kundennummer, strasse, hausnummer, plz, wohnort);
+            kundenService.kundeHinzufuegen(kunde);
+
+            showAlert("Erfolg","Kunde hinzugefügt");
+        });
+    }
+    private void showAlert(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void showAddBestellung() {
+
     }
 
     public static void main(String[] args) {
